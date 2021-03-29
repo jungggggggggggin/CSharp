3e3060c8d721b0356bc322dd2c5011308f576a2a
jungggggggggggin <37722217+jungggggggggggin@users.noreply.github.com>
2021-03-29 10:06:12 +0900

    CSharpStatement
    CSharpStatment 예제 2021.03.29

---
+++ CSharpStatement/10ForStApp.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+namespace Statement
+{
+    class ForStApp
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            double h = 0.0;
+            Console.Write("Enter a number = ");
+            n = Console.Read() - '0';
+            for (i = 1; i <= n; ++i)
+                h = h + 1 / (double)i;
+            Console.WriteLine("n = {0}, h = {1}", n, h);
+
+        }
+    }
+}
---
+++ CSharpStatement/13DoWhileStApp.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace Statement
+{
+    class DoWhileStApp
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            double h = 0.0;
+            Console.Write("Enter a number = ");
+            n = Console.Read() - '0';
+            i = 1;
+            do
+            {
+                h = h + 1 / (double)i;
+                i++;
+            } while (i <= n);
+            Console.WriteLine("n = " + n + ", h = " + h);
+        }
+    }
+}
